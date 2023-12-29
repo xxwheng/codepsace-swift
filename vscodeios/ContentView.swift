b8c711c93b7a3c0bf62b7af4a5562229737725ad
wangheng <wangheng@yungengxin.com>
2023-12-29 11:04:41 +0800

    init

---
+++ vscodeios/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  vscodeios
+//
+//  Created by wheng on 2023/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Spacer()
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vscodeios/vscodeiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  vscodeiosApp.swift
+//  vscodeios
+//
+//  Created by wheng on 2023/12/29.
+//
+
+import SwiftUI
+
+@main
+struct vscodeiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
